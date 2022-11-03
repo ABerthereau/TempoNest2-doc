2f74cf634b74992c5c6cdb1deaa762ae711d23cf
ABerthereau <60774344+ABerthereau@users.noreply.github.com>
2022-11-03 13:33:15 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../../TempoNest2/'))
+sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'TempoNest2'
